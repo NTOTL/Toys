a9da5896e83cad944f0fcc1bb387e51653f5e819
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2022-04-11 12:26:42 -0500

    ViewProduct.aspx initial release

---
+++ Toys/ViewProduct.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ViewProduct.aspx.cs" Inherits="Toys.ViewProduct" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>View Product</h3>
+    <div class="row">
+        <div class="col-sm-3">
+            <asp:Image ID="imgProduct" runat="server" />
+        </div>
+        <div class="col-sm-2">
+            <asp:Label ID="lblProductName" runat="server">Product: </asp:Label>
+            <asp:Label ID="lblProductDescription" runat="server">Description: </asp:Label>
+            <br />
+            <asp:Label ID="lblUnitPrice" runat="server">Unit price: </asp:Label>
+            <br />
+            <asp:Label ID="lblCategoryName" runat="server">Category: </asp:Label>
+            <br />
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <asp:Button ID="btnReturn" runat="server" Text="Go Back" CssClass="btn btn-primary" />
+        </div>
+    </div>
+</asp:Content>
---
+++ Toys/ViewProduct.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Toys
+{
+    public partial class ViewProduct : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                if (Request.QueryString["productId"] != null)
+                {
+                    int productId = int.Parse(Request.QueryString["productId"]);
+                    using (SqlConnection conn = new SqlConnection())
+                    {
+                        conn.ConnectionString = WebConfigurationManager.ConnectionStrings["ToysConnectionString"].ConnectionString;
+                        SqlCommand cmd = new SqlCommand();
+                        cmd.CommandText = "SELECT ProductName, Description, UnitPrice, ImagePath, CategoryName FROM Products P INNER JOIN Categories C " +
+                            "ON P.CategoryId = C.CategoryId  WHERE P.ProductId = " + productId ;
+
+                        cmd.Connection = conn;
+                        conn.Open();
+
+                        SqlDataReader sdr = cmd.ExecuteReader();
+
+                        if (sdr.HasRows)
+                        {
+                            if (sdr.Read())
+                            {
+                                lblProductName.Text += sdr["ProductName"].ToString();
+                                lblProductDescription.Text += sdr["Description"].ToString();
+                                lblUnitPrice.Text += string.Format("{0:c}", sdr["UnitPrice"]);
+                                imgProduct.ImageUrl = "/Content/Catalog/Images/" + sdr["ImagePath"].ToString();
+                                lblCategoryName.Text += sdr["CategoryName"].ToString();
+
+                            }
+
+                        }
+                            
+                    }
+                }
+            }
+        }
+    }
+}
