1531dac9c6b87c39a3ed6604adfa78c8d465275f
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2022-04-13 15:39:06 -0500

    AddProduct.aspx

---
+++ Toys/AddProduct.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddProduct.aspx.cs" Inherits="Toys.AddProduct" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h3>Add New Product</h3>
+    <div class="form-group">
+        <label for="txtProductName">Product Name:</label>
+        <asp:TextBox ID="txtProductName" runat="server" ClientIDMode="Static" CssClass="form-control"></asp:TextBox>
+        <label for="txtProductDescription">Description:</label>
+        <asp:TextBox ID="txtProductDescription" runat="server" ClientIDMode="Static" CssClass="form-control" TextMode="MultiLine" Rows="3">
+        </asp:TextBox>
+        <label for="txtUnitPrice">Price:</label>
+        <asp:TextBox ID="txtUnitPrice" runat="server" ClientIDMode="Static" CssClass="form-control"></asp:TextBox>
+        <label for="fuProductImage">Product Image: </label>
+        <asp:FileUpload ID="fuProductImage" runat="server" ClientIDMode="Static" CssClass="form-control-file" />
+        <label for="ddlCategories">Category: </label>
+        <asp:DropDownList ID="ddlCategories" runat="server" ClientIDMode="Static" CssClass="form-control"></asp:DropDownList>
+    </div>
+    <div class="form-group">
+        <asp:Button ID="btnAddProduct" runat="server" ClientIDMode="Static" CssClass ="btn btn-primary" Text="Add Product" OnClick="btnAddProduct_Click" />
+    </div>
+</asp:Content>
---
+++ Toys/AddProduct.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Toys
+{
+    public partial class AddProduct : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindCategoryDDL();
+            }
+        }
+
+        private void BindCategoryDDL()
+        {
+            using(SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = WebConfigurationManager.ConnectionStrings["ToysConnectionString"].ConnectionString;
+
+                // 2. Create a SqlCommand object
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "SELECT * FROM Categories ORDER BY CategoryName";
+                cmd.Connection = conn;
+
+                SqlDataAdapter sda = new SqlDataAdapter();
+                sda.SelectCommand = cmd;
+
+                DataTable dt = new DataTable();
+                conn.Open();
+
+                sda.Fill(dt);
+
+                ddlCategories.DataSource = dt;
+                ddlCategories.DataTextField = "CategoryName";
+                ddlCategories.DataValueField = "CategoryId";
+                ddlCategories.DataBind();
+            }
+        }
+
+        protected void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                string imagePath = "";
+                if (fuProductImage.HasFile)
+                {
+                    imagePath = fuProductImage.FileName;
+                    fuProductImage.SaveAs(Server.MapPath(Request.ApplicationPath) + "/Content/Catalog/Images/" + imagePath);
+
+                    using (SqlConnection conn = new SqlConnection())
+                    {
+                        conn.ConnectionString = WebConfigurationManager.ConnectionStrings["ToysConnectionString"].ConnectionString;
+
+                        // 2. Create a SqlCommand object
+                        SqlCommand cmd = new SqlCommand();
+                        ///TODO
+                        cmd.CommandText = "INSERT INTO Products VALUES('" + txtProductName.Text.Trim() + "', '" +
+                            txtProductDescription.Text.Trim() + "', '" + imagePath + "', " + txtUnitPrice.Text.Trim() +
+                            ", " + ddlCategories.SelectedValue + ")";
+                        cmd.Connection = conn;
+                        conn.Open();
+
+                        cmd.ExecuteNonQuery();
+
+                        Response.Redirect("Default.aspx");
+                    }
+                }
+            }
+        }
+    }
+}
